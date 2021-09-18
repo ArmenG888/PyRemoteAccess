c9f89c1bdc60f9c090371c775331e3597dd3f98a
Armen._.G <52843550+ArmenG888@users.noreply.github.com>
2021-09-18 10:16:40 -0700

    keyboard and mouse connected

--- PyRemoteAccess/mouse_control_client.py
+++ PyRemoteAccess/mouse_control_client.py
@@ -1,6 +1,8 @@
+from threading import Thread
 import socket
 import keyboard
-
+import pyautogui
+import time
 ip_port = ("192.168.1.2", 52000)
 class client:
     def __init__(self, ip_port):
@@ -8,6 +10,18 @@ class client:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.connect(ip_port)
         while True:
-            key = s.recv(1024).decode()
-            pyautogui.moveTo(key[0],key[1])
+            Thread(target = self.mouse).start()
+            Thread(target = self.keyboard).start()
+    def mouse(self):
+        x = s.recv(1024).decode()
+        y = s.recv(1024).decode()
+        pyautogui.moveTo(x,y)
+    def keyboard(self):
+        key = s.recv(1024).decode()
+        if "'" in key:
+            key = key.replace("'", "",2)
+        elif "Key." in key:
+            key = key.replace("Key.", "")
+        print(key)
+        keyboard.press_and_release(key)
 app = client(ip_port)
